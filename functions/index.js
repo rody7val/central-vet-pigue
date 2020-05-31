5514bf4b154e45180b0f56c98363d7c01592fd6d
rody7val <rod7val@gmail.com>
2020-05-31 01:55:00 -0300

    success get init_point mp

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,8 @@ const mercadopago = require('mercadopago')
 
 admin.initializeApp(functions.config().firebase)
 mercadopago.configure({
-  client_id: '8942313801155865',
-  client_secret: 'eiPDPiTOxXCBI0WUf9C77Geh5oSPMxla',
-  access_token: 'TEST-8942313801155865-050202-af0fde1fc733a4152ca18e189d29dd9c-200036843'
+  client_id: "8942313801155865",
+  client_secret: "eiPDPiTOxXCBI0WUf9C77Geh5oSPMxla"
 })
 
 exports.welcome = functions.auth.user().onCreate( user => {
@@ -30,10 +29,11 @@ exports.createItem = functions.firestore.document('items/{id}').onCreate((snap, 
 })
 
 exports.createPreference = functions.https.onCall((data, context) => {
-  if (context.auth && data.items) {
-    console.log(data.items)
-    mercadopago.preferences.create(data).then(data => {
-      console.log(data)
+  if (data.items) {
+    let preference = { items: data.items }
+    console.log(preference)
+    return mercadopago.preferences.create(preference).then(data => {
+      console.log(data.body)
       return { success: true, init_point: data.body.init_point }
     }).catch(error => {
       console.log(error)
@@ -43,3 +43,20 @@ exports.createPreference = functions.https.onCall((data, context) => {
     return {success: false}
   }
 });
+
+exports.createPreference = functions.https.onCall((data, context) => {
+  if (data.items) {
+    let preference = { items: data.items }
+    console.log(preference)
+    return mercadopago.preferences.create(preference).then(data => {
+      console.log(data.body)
+      return { success: true, init_point: data.body.init_point }
+    }).catch(error => {
+      console.log(error)
+      return { success: false, error: error }
+    })
+  } else {
+    return {success: false}
+  }
+});
+
